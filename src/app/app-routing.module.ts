b32761869292d79691bf35505078967d9f40d252
JoshitaRavichandaran <joshi10ravi@gmail.com>
2023-02-16 10:36:33 +0530

    added homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
+  {path : "", component : HomeComponent},
   {path : "login", component : LoginComponent},
   {path : "signup", component : SignupComponent},
   {path : "products", component : ProductListComponent},
